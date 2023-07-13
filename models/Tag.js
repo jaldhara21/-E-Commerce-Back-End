25c1fb81c3ecd72de159e052669a81c4c1c0b7f8
jaldhara.patel <jaldharapatel91@gmail.com>
2023-07-13 11:28:58 -0400

    Define columns for the tag table

--- models/Tag.js
+++ models/Tag.js
@@ -1,19 +1,29 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    // define columns for the tag table
+    id: {
+      type: DataTypes.INTEGER, // Data type is integer
+      allowNull: false, // Attribute cannot be null
+      primaryKey: true, // Attribute is the primary key
+      autoIncrement: true, // Attribute automatically increments
+    },
+    // Define the tag_name attribute
+    tag_name: {
+      type: DataTypes.STRING, // Data type is string
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: "tag",
   }
 );
 

4c698cb5c5f0f94660ecd06846853fc84a0385e2
jaldhara.patel <jaldharapatel91@gmail.com>
2023-07-13 11:28:34 -0400

    Define the columns

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,38 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+     // Define columns for the product table
+     id: {
+      type: DataTypes.INTEGER, // Data type is integer
+      allowNull: false, // Attribute cannot be null
+      primaryKey: true, // Attribute is the primary key
+      autoIncrement: true, // Attribute automatically increments
+    },
+    product_name: {
+      type: DataTypes.STRING, // Data type is string
+      allowNull: false, 
+    },
+    price: {
+      type: DataTypes.DECIMAL, // Data type is decimal
+      allowNull: false,
+      validate: {
+        isDecimal: true,  // Validate that the value is a decimal number
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      defaultValue: 10, // Set the default value to 10
+      validate: {
+        isNumeric: true, // Validate that the value is a numeric value
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER, 
+      references: {
+        model: "category", // Reference the 'category' table
+      },
+    },
   },
   {
     sequelize,

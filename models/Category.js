e0c1165adbeb143814defe15367ac5a67bc6f607
jaldhara.patel <jaldharapatel91@gmail.com>
2023-07-13 11:28:10 -0400

    Define the id attribute

--- models/Category.js
+++ models/Category.js
@@ -1,19 +1,30 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // Define the id attribute
+    id: {
+      type: DataTypes.INTEGER, // Data type is integer
+      allowNull: false, // Attribute cannot be null
+      primaryKey: true, // Attribute is the primary key
+      autoIncrement: true, // Attribute automatically increments
+    },
+
+    // Define the category_name attribute
+    category_name: {
+      type: DataTypes.STRING, // Data type is string
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 

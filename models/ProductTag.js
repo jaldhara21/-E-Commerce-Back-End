ce3b878c0936ebdce0964ed01c8366cbae3c034d
jaldhara.patel <jaldharapatel91@gmail.com>
2023-07-13 18:19:37 -0400

    Define columns for the product_tag table

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,19 +1,42 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // Define columns for the product_tag table
+    // Column: id
+    id: {
+      type: DataTypes.INTEGER, // Data type is integer
+      allowNull: false, // Attribute cannot be null
+      primaryKey: true, // Attribute is the primary key
+      autoIncrement: true, // Attribute automatically increments
+    },
+    // Column: tag_id
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag", // Reference the 'tag' table
+        key: "id", // Reference the 'id' column in the 'tag' table
+      },
+    },
+    // Column: product_id
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product", // Reference the 'product' table
+        key: "id", // Reference the 'id' column in the 'product' table
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 

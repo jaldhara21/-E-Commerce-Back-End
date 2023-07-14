e5ab1a981ab4a7804803d301bb1105974cae17a4
jaldhara.patel <jaldharapatel91@gmail.com>
2023-07-13 21:49:29 -0400

    Added comments

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
+// Importing the routes for categories, products, and tags
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
-
+// Use the category, product, and tag route modules for their endpoints
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
 
+// Exporting the router
 module.exports = router;

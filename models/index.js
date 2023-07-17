beb3135dcfa49f94b5c2d3e3d8c1f13f553d3f41
jaldhara.patel <jaldharapatel91@gmail.com>
2023-07-17 09:44:31 -0400

    Removed forignkey from belongstomany

--- models/index.js
+++ models/index.js
@@ -1,30 +1,27 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id', // The foreign key in the Product model referencing the Category model
+  foreignKey: "category_id", // The foreign key in the Product model referencing the Category model
 });
 
-// Caegories have many Products
 // A Product belongs to many Tags through the ProductTag model
 Product.belongsToMany(Tag, {
   through: ProductTag, // The intermediate model representing the many-to-many relationship between Product and Tag
-  foreignKey: 'product_id', // The foreign key in the Product model referencing the ProductTag model
 });
 
 // A Tag belongs to many Products through the ProductTag model
 Tag.belongsToMany(Product, {
   through: ProductTag, // The intermediate model
-  foreignKey: 'tag_id', // The foreign key in the Tag model
 });
 
 // A Category has many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_id', // The foreign key in the Product model
+  foreignKey: "category_id", // The foreign key in the Product model
 });
 
 module.exports = {
